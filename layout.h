5d9561f243f7b4b6beb5b4a36368c6bde3dca749
Timothy Davis <tmthydvs@gmail.com>
2023-09-12 09:53:17 +0930

    Use numbers, not keypad numbers

--- layout.h
+++ layout.h
@@ -32,9 +32,9 @@
                            MO(L_NAV), KC_TRNS, KC_TRNS, KC_TRNS
 
 #define _NUM \
-  KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_NO  , KC_P7  , KC_P8  , KC_P9  , KC_NO  , \
-  KC_NO  , KC_F4  , KC_F5  , KC_F6  , KC_F11 , KC_P0  , KC_P4  , KC_P5  , KC_P6  , KC_NO  , \
-  KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F10 , KC_UNDS, KC_P1  , KC_P2  , KC_P3  , KC_NO  , \
+  KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_NO  , KC_7   , KC_8   , KC_9   , KC_NO  , \
+  KC_NO  , KC_F4  , KC_F5  , KC_F6  , KC_F11 , KC_0   , KC_4   , KC_5   , KC_6   , KC_NO  , \
+  KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F10 , KC_UNDS, KC_1   , KC_2   , KC_3   , KC_NO  , \
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 
 #define LAYOUT_a_dux_wrap(...) LAYOUT_split_3x5_2(__VA_ARGS__)
