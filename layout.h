3ba9d9ced732b54fe2317664653b8423c40d2556
Timothy Davis <tmthydvs@gmail.com>
2023-09-13 09:04:17 +0930

    Add comma and period to number layer

--- layout.h
+++ layout.h
@@ -44,8 +44,8 @@
 
 #define _NUM \
   KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_NO  , KC_7   , KC_8   , KC_9   , D_GAME , \
-  KC_NO  , KC_F4  , KC_F5  , KC_F6  , KC_F11 , KC_0   , KC_4   , KC_5   , KC_6   , KC_DOT , \
-  KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F10 , KC_UNDS, KC_1   , KC_2   , KC_3   , KC_NO  , \
+  KC_NO  , KC_F4  , KC_F5  , KC_F6  , KC_F11 , KC_0   , KC_4   , KC_5   , KC_6   , KC_COMM, \
+  KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F10 , KC_UNDS, KC_1   , KC_2   , KC_3   , KC_DOT , \
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 
 #define _GAME \
