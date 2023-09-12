972804a8d1a8f771efc555e1c6d422e2a12b2208
Timothy Davis <tmthydvs@gmail.com>
2023-09-12 13:23:17 +0930

    Fix typo in layout.h

--- layout.h
+++ layout.h
@@ -39,13 +39,13 @@
                              L_NAV  , KC_TRNS, KC_TRNS, KC_TRNS
 
 #define _NUM \
-  KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_NO  , KC_7   , KC_8   , KC_9   , R_GAME , \
+  KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_NO  , KC_7   , KC_8   , KC_9   , D_GAME , \
   KC_NO  , KC_F4  , KC_F5  , KC_F6  , KC_F11 , KC_0   , KC_4   , KC_5   , KC_6   , KC_DOT , \
   KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F10 , KC_UNDS, KC_1   , KC_2   , KC_3   , KC_NO  , \
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 
 #define _GAME \
-  KC_TAB , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , R_BASE , \
+  KC_TAB , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , D_BASE , \
   KC_LSFT, KC_A   , KC_W   , KC_D   , KC_E   , KC_R   , KC_T   , KC_NO  , KC_NO  , KC_NO  , \
   KC_LCTL, KC_Z   , KC_S   , KC_X   , KC_C   , KC_V   , KC_B   , KC_NO  , KC_NO  , KC_NO  , \
                              KC_LALT, KC_SPC , KC_NO  , KC_NO
