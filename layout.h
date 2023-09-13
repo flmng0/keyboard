aff25ff513705900dfc204b12160975cc6ab7ab9
Timothy Davis <tmthydvs@gmail.com>
2023-09-14 09:23:26 +0930

    Realised I was using wrong keycode

--- layout.h
+++ layout.h
@@ -22,7 +22,7 @@
 #define CT_X CT(KC_X)
 #define CT_Z CT(KC_Z)
 
-#define CT_RETN CT(KC_RETN)
+#define CT_ENT CT(KC_ENT)
 
 #define _BASE \
   KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, \
@@ -32,13 +32,13 @@
 
 #define _NAV \
   KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_ESC , KC_DEL , KC_HOME, KC_PGDN, KC_PGUP, KC_END , \
-  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_TAB , CT_RETN, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, \
+  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_TAB , CT_ENT , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, \
   KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO  , KC_BSPC, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, \
                              KC_TRNS, KC_TRNS, KC_TRNS, L_SYM
 
 #define _SYM \
   KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_SLSH, KC_COLN, \
-  KC_TILD, KC_GRV , KC_DQUO, KC_BSLS, KC_NO  , CT_RETN, KC_PIPE, KC_PLUS, KC_MINS, KC_QUES, \
+  KC_TILD, KC_GRV , KC_DQUO, KC_BSLS, KC_NO  , CT_ENT , KC_PIPE, KC_PLUS, KC_MINS, KC_QUES, \
   KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO  , KC_NO  , KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, \
                              L_NAV  , KC_TRNS, KC_TRNS, KC_TRNS
 
