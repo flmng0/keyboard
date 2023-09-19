5f97059b7267566e70dbf1dccea629552f7b7544
Timothy Davis <tmthydvs@gmail.com>
2023-09-19 15:12:39 +0930

    Embed BRMs into _BASE

--- layout.h
+++ layout.h
@@ -15,21 +15,20 @@
 #define L_NAV LT_NAV(KC_TAB)
 #define L_SYM LT_SYM(KC_ENTER)
 
-/* Macro to apply bottom-row-mods (BRMs) */
-#define BRM( \
-    AA, BA, CA, DA, EA, FA, GA, HA, IA, JA, \
-    AB, BB, CB, DB, EB, FB, GB, HB, IB, JB, \
-    AC, BC, CC, DC, EC, FC, GC, HC, IC, JC, \
-                TA, TB, TC, TE)            \
-    AA        , BA        , CA        , DA        , EA  , FA  , GA        , HA        , IA        , JA        , \
-    AB        , BB        , CB        , DB        , EB  , FB  , GB        , HB        , IB        , JB        , \
-    LGUI_T(AC), LALT_T(BC), LSFT_T(CC), LCTL_T(DC), EC  , FC  , RCTL_T(GC), RSFT_T(HC), LALT_T(IC), RGUI_T(JC), \
-                                                TA, TB  , TC  , TE
+#define BM_Z LGUI_T(KC_Z)
+#define BM_X LALT_T(KC_X)
+#define BM_C LSFT_T(KC_C)
+#define BM_D LCTL_T(KC_D)
+
+#define BM_H    RCTL_T(KC_H)
+#define BM_COMM RSFT_T(KC_COMM)
+#define BM_DOT  LALT_T(KC_DOT)
+#define BM_QUOT RGUI_T(KC_QUOT)
 
 #define _BASE \
   KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, \
   KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , \
-  KC_Z   , KZ_X   , KC_C   , KC_D   , KC_V   , KC_K   , KC_H   , KC_COMM, KC_DOT , KC_QUOT, \
+  BM_Z   , BM_X   , BM_C   , BM_D   , BM_V   , KC_K   , BM_H   , BM_COMM, BM_DOT , BM_QUOT, \
                              L_NAV  , KC_SPC , KC_BSPC, L_SYM
 
 #define _NAV \
@@ -40,14 +39,14 @@
 
 #define _SYM \
   KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_SLSH, KC_COLN, \
-  KC_TILD, KC_GRV , KC_DQUO, KC_BSLS, KC_NO  , CT_ENT , KC_PIPE, KC_PLUS, KC_MINS, KC_QUES, \
-  KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO  , KC_NO  , KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, \
+  KC_TILD, KC_GRV , KC_DQUO, KC_BSLS, KC_NO  , KC_NO  , KC_PIPE, KC_PLUS, KC_MINS, KC_QUES, \
+  KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO  , KC_ENT , KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, \
                              L_NAV  , KC_TRNS, KC_TRNS, KC_TRNS
 
 #define _NUM \
-  KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_RET , KC_7   , KC_8   , KC_9   , D_GAME , \
+  KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_UNDS, KC_7   , KC_8   , KC_9   , D_GAME , \
   KC_NO  , KC_F4  , KC_F5  , KC_F6  , KC_F11 , KC_0   , KC_4   , KC_5   , KC_6   , KC_COMM, \
-  KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F10 , KC_UNDS, KC_1   , KC_2   , KC_3   , KC_DOT , \
+  KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F10 , KC_ENT , KC_1   , KC_2   , KC_3   , KC_DOT , \
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 
 #define _GAME \
