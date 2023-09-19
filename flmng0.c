0e97da177f9e1e82c8b67e3aa55558671600e1e0
Timothy Davis <tmthydvs@gmail.com>
2023-09-19 14:57:04 +0930

    Heavily modified

--- flmng0.c
+++ flmng0.c
@@ -15,13 +15,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 const uint16_t PROGMEM c_lparen[] = {KC_T, KC_D, COMBO_END};
 const uint16_t PROGMEM c_rparen[] = {KC_N, KC_H, COMBO_END};
 
-const uint16_t PROGMEM c_lbracket[] = {KC_S, CT_C, COMBO_END};
+const uint16_t PROGMEM c_lbracket[] = {KC_S, KC_C, COMBO_END};
 const uint16_t PROGMEM c_rbracket[] = {KC_E, KC_COMMA, COMBO_END};
 
-const uint16_t PROGMEM c_lbrace[] = {KC_G, CT_V, COMBO_END};
+const uint16_t PROGMEM c_lbrace[] = {KC_G, KC_V, COMBO_END};
 const uint16_t PROGMEM c_rbrace[] = {KC_M, KC_K, COMBO_END};
 
-const uint16_t PROGMEM c_langle[] = {KC_R, CT_X, COMBO_END};
+const uint16_t PROGMEM c_langle[] = {KC_R, KC_X, COMBO_END};
 const uint16_t PROGMEM c_rangle[] = {KC_I, KC_DOT, COMBO_END};
 
 /**
@@ -29,24 +29,7 @@ const uint16_t PROGMEM c_rangle[] = {KC_I, KC_DOT, COMBO_END};
  */
 const uint16_t PROGMEM c_hyphen[] = {KC_B, KC_J, COMBO_END};
 const uint16_t PROGMEM c_equals[] = {KC_G, KC_M, COMBO_END};
-const uint16_t PROGMEM c_underscore[] = {CT_V, KC_K, COMBO_END};
-
-/**
- * Left Side:
- * FP => Esc
- * ST => Tab
- *
- * Right Side:
- * LU => Delete
- * NE => Return
- * H, = Backspace
- */
-const uint16_t PROGMEM c_escape[] = {KC_F, KC_P, COMBO_END};
-const uint16_t PROGMEM c_tab[] = {KC_S, KC_T, COMBO_END};
-
-const uint16_t PROGMEM c_delete[] = {KC_L, KC_U, COMBO_END};
-const uint16_t PROGMEM c_return[] = {KC_N, KC_E, COMBO_END};
-const uint16_t PROGMEM c_backspace[] = {KC_H, KC_COMMA, COMBO_END};
+const uint16_t PROGMEM c_underscore[] = {KC_V, KC_K, COMBO_END};
 
 combo_t key_combos[] = {
   // Parenthesis
@@ -66,69 +49,5 @@ combo_t key_combos[] = {
   COMBO(c_hyphen, KC_MINUS),
   COMBO(c_equals, KC_EQUAL),
   COMBO(c_underscore, LSFT(KC_MINUS)),
-
-  // Functional buttons
-  COMBO(c_escape, KC_ESCAPE),
-  COMBO(c_tab, KC_TAB),
-
-  COMBO(c_delete, KC_DELETE),
-  COMBO(c_return, CT_ENT),
-  COMBO(c_backspace, KC_BACKSPACE),
 };
 
-/**
- * Check whether a certain keycode is a "Control Tap" keycode.
- *
- * We use LT(0, <code>) to represent a Control Tap, so we need to check:
- *  1. Is the code a Layer Tap:
- *    ((code & QK_LAYER_TAP) != 0)
- *  2. Is the layer for that layer tap equal to 0:
- *    (((code >> 8) & 0xF) == 0)
- */
-#define IS_CT(code) (code & QK_LAYER_TAP) && (((code >> 8) & 0xF) == 0)
-
-/**
- * Extract the desired key from a "Control Tap" keycode.
- *
- * The key is stored in the right-most 8-bits in a Layer-Tap code.
- */
-#define CT_KC(code) C(code & 0xFF)
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (IS_CT(keycode)) {
-    if (!record->tap.count && record->event.pressed) {
-      uint16_t new_code = CT_KC(keycode);
-      tap_code16(new_code);
-      return false;
-    }
-  }
-
-  return true;
-}
-
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-  // With super-light key switches, it's easy to unintentially hold down a key
-  // for a little bit with heavy fingers.
-  //
-  // We don't wont to activate control-tab keys by accident.
-  if (IS_CT(keycode)) {
-    return TAPPING_TERM + 400;
-  }
-
-  return TAPPING_TERM;
-}
-
-uint16_t get_combo_term(uint16_t index, combo_t *combo) {
-  // Since return and tab combos are on the home row, the movement to roll-over
-  // is quite fast, and would be frustrating to misfire.
-  //
-  // 25 is still possible to misfire, but harder, and is definitely possible to
-  // do on purpose.
-  switch (combo->keycode) {
-    case CT(KC_RETN):
-    case KC_TAB:
-      return 25;
-  }
-
-  return COMBO_TERM;
-}
--- layout.h
+++ layout.h
@@ -9,41 +9,43 @@
 #define D_BASE DF(ID_BASE)
 #define D_GAME DF(ID_GAME)
 
-#define L_NAV MO(ID_NAV)
-#define L_SYM MO(ID_SYM)
+#define LT_NAV(code) LT(ID_NAV, code)
+#define LT_SYM(code) LT(ID_SYM, code)
 
-#define CT(code) LT(0, code)
+#define L_NAV LT_NAV(KC_TAB)
+#define L_SYM LT_SYM(KC_ENTER)
 
-/**
- * Control tap key definitions
- */
-#define CT_C CT(KC_C)
-#define CT_V CT(KC_V)
-#define CT_X CT(KC_X)
-#define CT_Z CT(KC_Z)
-
-#define CT_ENT CT(KC_ENT)
+/* Macro to apply bottom-row-mods (BRMs) */
+#define BRM( \
+    AA, BA, CA, DA, EA, FA, GA, HA, IA, JA \
+    AB, BB, CB, DB, EB, FB, GB, HB, IB, JB \
+    AC, BC, CC, DC, EC, FC, GC, HC, IC, JC \
+                TA, TB, TC, TE)            \
+    AA        , BA        , CA        , DA        , EA  , FA  , GA        , HA        , IA        , JA         \
+    AB        , BB        , CB        , DB        , EB  , FB  , GB        , HB        , IB        , JB         \
+    LGUI_T(AC), LALT_T(BC), LSFT_T(CC), LCTL_T(DC), EC  , FC  , RCTL_T(GC), RSFT_T(HC), LALT_T(IC), RGUI_T(JC) \
+                                                TA, TB  , TC  , TE
 
 #define _BASE \
   KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, \
   KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , \
-  CT_Z   , CT_X   , CT_C   , KC_D   , CT_V   , KC_K   , KC_H   , KC_COMM, KC_DOT , KC_QUOT, \
-                             L_NAV  , KC_SPC , KC_LSFT, L_SYM
+  KC_Z   , KZ_X   , KC_C   , KC_D   , KC_V   , KC_K   , KC_H   , KC_COMM, KC_DOT , KC_QUOT, \
+                             L_NAV  , KC_SPC , KC_BSPC, L_SYM
 
 #define _NAV \
-  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_ESC , KC_DEL , KC_HOME, KC_PGDN, KC_PGUP, KC_END , \
-  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_TAB , CT_ENT , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, \
-  KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO  , KC_BSPC, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, \
-                             KC_TRNS, KC_TRNS, KC_TRNS, L_SYM
+  KC_ESC , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_HOME, KC_PGDN, KC_PGUP, KC_END , \
+  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, \
+  KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO  , KC_NO  , KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, \
+                             KC_TRNS, KC_TRNS, KC_TRNS, LT_SYM(KC_DEL)
 
 #define _SYM \
   KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_SLSH, KC_COLN, \
   KC_TILD, KC_GRV , KC_DQUO, KC_BSLS, KC_NO  , CT_ENT , KC_PIPE, KC_PLUS, KC_MINS, KC_QUES, \
-  KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO  , KC_NO  , KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, \
+  KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO  , KC_NO  , KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, \
                              L_NAV  , KC_TRNS, KC_TRNS, KC_TRNS
 
 #define _NUM \
-  KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_NO  , KC_7   , KC_8   , KC_9   , D_GAME , \
+  KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_RET , KC_7   , KC_8   , KC_9   , D_GAME , \
   KC_NO  , KC_F4  , KC_F5  , KC_F6  , KC_F11 , KC_0   , KC_4   , KC_5   , KC_6   , KC_COMM, \
   KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F10 , KC_UNDS, KC_1   , KC_2   , KC_3   , KC_DOT , \
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
