7231d9333f4cbe314b2dafd32dcdfe3218166a4b
Timothy Davis <tmthydvs@gmail.com>
2023-09-19 16:12:30 +0930

    Add caps word to nav layer

--- layout.h
+++ layout.h
@@ -33,7 +33,7 @@
 
 #define _NAV \
   KC_ESC , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_HOME, KC_PGDN, KC_PGUP, KC_END , \
-  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, \
+  CW_TOGG, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, \
   KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO  , KC_NO  , KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, \
                              KC_TRNS, KC_TRNS, KC_TRNS, LT_SYM(KC_DEL)
 
