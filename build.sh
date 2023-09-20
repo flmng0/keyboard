8ba98b462ddaa167e5393e133ae2972e1b7f1c75
Timothy Davis <tmthydvs@gmail.com>
2023-09-20 15:07:32 +0930

    Generalize and fix build script

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
 dir="$HOME/qmk_firmware"
+user="flmng0"
 
 podman run --rm \
   --privileged \
 	-w /qmk_firmware \
 	-v "$dir":/qmk_firmware \
+  -v "$PWD":"/qmk_firmware/users/$user" \
 	ghcr.io/qmk/qmk_cli \
-	qmk compile -c "users/flmng0/keymaps/combo.json"
+	qmk compile -c "users/$user/keymaps/combo.json"
